a6628d98186c513a551d6a39578aed93f1773d23
Alex Entrepreneur <hello@alexentrepreneur.com>
2019-03-28 12:11:34 +0000

    Add Cohorts Endpoints Functionality

--- routers/cohorts-router.js
+++ routers/cohorts-router.js
@@ -2,32 +2,106 @@ const express = require("express");
 const knex = require("knex");
 const knexConfig = require('../knexfile');
 const db = knex(knexConfig.development);
+const Cohorts = db('cohorts');
+const Students = db('students');
 const router = express.Router();
 
 router.use(express.json());
 
 router.get("/", (req, res) => {
-  res.send('GET cohorts list');
+  Cohorts.select('id', 'name')
+  .then((cohortsArray) => {
+    res.json(cohortsArray);
+  })
+  .catch((err) => {
+    res.status(500).json({
+      message: 'Server failed to retrieve cohorts.',
+      serverMessage: `${err}`
+    })
+  });
 });
 
 router.get("/:id", (req, res) => {
-  res.send('GET cohort');
+  const { id } = req.params;
+  Cohorts.where({ id }).select('id', 'name').first()
+    .then((cohortObject) => {
+      res.json(cohortObject)
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: `Server failed to retrieve cohort of id ${id}.`,
+        serverMessage: `${err}`
+      })
+    });
 });
 
-router.get("/:id/students", (req, res) => {
-  res.send('GET cohort students');
+router.get("/:cohort_id/students", (req, res) => {
+  const { cohort_id } = req.params;
+  Students.where({ cohort_id }).select('id', 'name', 'cohort_id')
+    .then((studentsObject) => {
+      debugger
+      res.json(studentsObject)
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: `Server failed to retrieve students from cohort of id ${cohort_id}.`,
+        serverMessage: `${err}`
+      })
+    });
 });
 
 router.post("/", (req, res) => {
-  res.send('POST cohort');
+  const { name } = req.body;
+  const newCohortObject = { name };
+  Cohorts.insert(newCohortObject)
+    .then((newCohortId) => {
+      return Cohorts.select('id', 'name');
+    })
+    .then((CohortsArray) => {
+      res.json(CohortsArray);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: 'Server failed to create a new cohort.',
+        serverMessage: `${err}`
+      })
+    });
 });
 
 router.put("/:id", (req, res) => {
-  res.send('UPDATE cohort');
+  const { id } = req.params;
+  const { name } = req.body;
+  const newCohortObject = { name };
+  Cohorts.where({ id }).update(newCohortObject)
+    .then((updatedFieldsLength) => {
+      return Cohorts.select('id', 'name');
+    })
+    .then((CohortsArray) => {
+      res.json(CohortsArray);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: `Server failed to update cohort of id ${id}.`,
+        serverMessage: `${err}`
+      })
+    });
 });
 
 router.delete("/:id", async (req, res) => {
-  res.send('DELETE cohort');
+  const { id } = req.params;
+  Cohorts.where({ id }).del()
+    .then((deletedFieldsLength) => {
+      return Cohorts.select('id', 'name');
+    })
+    .then((CohortsArray) => {
+      res.json(CohortsArray);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: `Server failed to delete cohort of id ${id}.`,
+        serverMessage: `${err}`
+      })
+    });
 });
 
 module.exports = router;
